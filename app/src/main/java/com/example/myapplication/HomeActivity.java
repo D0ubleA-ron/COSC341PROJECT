25992fb16c7496360960df18911db81954a73f81
i_4m_h4ck3rm4n <piunger@hotmail.com>
2022-11-30 18:57:28 -0800

    started writing button click handlers and added a bunch of string arrays for the matching screen

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void ButtonClick(View view) {    // an interest button was pressed
+        Intent intent = new Intent(this, HomeActivity.class);   // CHANGE TO THE MATCHING SCREEN CLASS
+        intent.putExtra("index", 6);
+        //intent = new Intent(this, Matches_Screen.class);
+        startActivity(intent);
+    }
+
+    public void NavBarClick(View view) {    // a navbar button was clicked
+        String tag = view.getTag().toString();
+        Intent intent;
+        if (tag.equals("home"))     // home was clicked
+            intent = new Intent(this, HomeActivity.class);
+//        else if (tag.equals("messages"))    // messages was clicked
+//            intent = new Intent(this, );
+        else if (tag.equals("matches")) // matches was clicked
+            intent = new Intent(this, Matches_Screen.class);
+        else
+            intent = new Intent(this, Matches_Screen.class);
+//        else  // settings was clicked
+//            intent = new Intent(this, );
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/Matches_Screen.java
+++ app/src/main/java/com/example/myapplication/Matches_Screen.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Matches_Screen extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class Matches_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches_screen);
+    }
+
+    public void ButtonClick(View view) {    // a profile image was clicked
+        Intent intent = new Intent(this, HomeActivity.class);       // CHANGE TO THE MATCHING SCREEN CLASS
+        int index = Integer.parseInt(view.getTag().toString());
+        intent.putExtra("index", index);   // put the profile's index as the extra
+        startActivity(intent);
+    }
+
+    public void NavBarClick(View view) {    // a navbar button was clicked
+        String tag = view.getTag().toString();
+        Intent intent;
+        if (tag.equals("home"))     // home was clicked
+            intent = new Intent(this, HomeActivity.class);
+//        else if (tag.equals("messages"))    // messages was clicked
+//            intent = new Intent(this, );
+        else if (tag.equals("matches")) // matches was clicked
+            intent = new Intent(this, Matches_Screen.class);
+        else
+            intent = new Intent(this, HomeActivity.class);
+//        else  // settings was clicked
+//            intent = new Intent(this, );
+        startActivity(intent);
     }
 }
